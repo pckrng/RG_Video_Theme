6512c0158d0bed7f79b3a38c1537e370b7fe9c10
pckrng <pckrng@outlook.com>
2021-01-15 16:40:37 +0000

    script update and menu final

--- scripts.js
+++ scripts.js
@@ -3,4 +3,16 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
-            
+  
+	function myFunction(x) {
+			x.classList.toggle("change");
+		}
+		
+		function toogleSlideMenu(x){
+      if(x.classList.contains('change')){
+      	document.getElementById('myNav').style.width = '100%';
+      }
+      else{
+      	document.getElementById('myNav').style.width = '0px';
+      }
+		}
